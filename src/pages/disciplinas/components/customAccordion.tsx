31846aadd71756df7a1cda6f502f768dcc1b95df
Bruno Breyer <brunobreyergarcia@gmail.com>
2022-11-23 01:04:44 -0300

    WIP

--- src/pages/disciplinas/components/customAccordion.tsx
+++ src/pages/disciplinas/components/customAccordion.tsx
@@ -7,41 +7,67 @@ import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 import IconButton from '@mui/material/IconButton';
+import { IDisciplina } from '../disciplinas';
+import { useState } from 'react';
 
-export default function CusmtomAccordion() {
+const CusmtomAccordion = (disciplinas: any) => {
+  var listaDisciplinas: IDisciplina[] = disciplinas.disciplinas;
+
+  const [disciplinasState, setDisciplinaState] =
+    useState<IDisciplina[]>(listaDisciplinas);
+
+  const handleDelete = (id: string) => {
+    const disciplinas = disciplinasState.filter((disciplina) => disciplina.id !== id);
+    setDisciplinaState(disciplinas);
+  };
+
   return (
-    <div style={{ marginTop: "10px" }}>
-      <Accordion>
-        <AccordionSummary
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Grid container>
-            <Grid item xs={10.5}>
-              < Typography variant='h6'>Disciplina X</Typography>
-            </Grid>
-            <Grid item xs={1.5}>
-              <IconButton>
-                <EditIcon onClick={() => alert("Edit Me!")} />
-              </IconButton>
-              <IconButton>
-                <DeleteIcon color='warning' onClick={() => alert("Delete Me!")} />
-              </IconButton>
-            </Grid>
-            <Grid item xs={10}>
-              <Typography variant='subtitle1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget.</Typography>
-            </Grid>
-            <Grid item xs={2} />
-          </Grid>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
+    <div style={{ marginTop: '10px' }}>
+      {disciplinasState.map((dis, i) => {
+        return (
+          <Accordion key={i}>
+            <AccordionSummary
+              aria-controls='panel1a-content'
+              id='panel1a-header'
+            >
+              <Grid container>
+                <Grid item xs={10.5}>
+                  <Typography variant='h6'>{`${dis.nome} - ${dis.creditos} créditos`}</Typography>
+                </Grid>
+                <Grid item xs={1.5}>
+                  <IconButton onClick={() => alert('Edit Me!')}>
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton onClick={() => handleDelete(dis.id)}>
+                    <DeleteIcon color='warning' />
+                  </IconButton>
+                </Grid>
+                <Grid item xs={10}>
+                  <b>Programa da Disciplina:</b>
+                  <Typography variant='subtitle1'>{dis.programa}</Typography>
+                  <b>Itens Bibliográficos:</b>
+                  {dis.itensBlibliograficos.map((item, i) => {
+                    return <div key={i}>{`- ${item}`}</div>;
+                  })}
+                </Grid>
+                <Grid item xs={2} />
+              </Grid>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                <b>CodCred:</b> {dis.curriculo.idCurriculo}
+                <br />
+                <b>Curso:</b> {dis.curriculo.nomeCurso}
+                <br />
+                <b>Período:</b> {dis.curriculo.dataInicioVigencia} -{' '}
+                {dis.curriculo.dataFimVigencia}
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
     </div>
   );
-}
+};
+
+export default CusmtomAccordion;
--- src/pages/disciplinas/disciplinas.tsx
+++ src/pages/disciplinas/disciplinas.tsx
@@ -5,8 +5,140 @@ import { paths } from 'routes/routes';
 import CusmtomAccordion from './components/customAccordion';
 import SearchHeader from './components/searchHeader';
 
+export interface ICurriculo {
+  idCurriculo: number;
+  nomeCurso: string;
+  dataInicioVigencia: string;
+  dataFimVigencia: string;
+}
+
+export interface IDisciplina {
+  id: string;
+  nome: string;
+  creditos: number;
+  programa: string;
+  itensBlibliograficos: string[];
+  curriculo: ICurriculo;
+}
+
 export default function Disciplinas() {
   const history = useHistory();
+
+  const disciplinas: IDisciplina[] = [
+    {
+      id: '1',
+      nome: 'Construção de Software',
+      creditos: 4,
+      programa: 'Programa da disciplina',
+      itensBlibliograficos: [
+        'Livro de Engenharia de Software 1',
+        'Livro de Engenharia de Software 2',
+      ],
+      curriculo: {
+        idCurriculo: 100,
+        nomeCurso: 'Engenharia de Software',
+        dataInicioVigencia: '01/01/2021',
+        dataFimVigencia: '31/12/2021',
+      },
+    },
+    {
+      id: '2',
+      nome: 'Engenharia Experimental de Software',
+      creditos: 2,
+      programa: 'Programa da disciplina',
+      itensBlibliograficos: [
+        'Livro de Engenharia de Software 1',
+        'Livro de Engenharia de Software 2',
+      ],
+      curriculo: {
+        idCurriculo: 100,
+        nomeCurso: 'Engenharia de Software',
+        dataInicioVigencia: '01/01/2021',
+        dataFimVigencia: '31/12/2021',
+      },
+    },
+    {
+      id: '3',
+      nome: 'Paralela e Distribuida',
+      creditos: 2,
+      programa: 'Programa da disciplina',
+      itensBlibliograficos: [
+        'Livro de Engenharia de Software 1',
+        'Livro de Engenharia de Software 2',
+      ],
+      curriculo: {
+        idCurriculo: 100,
+        nomeCurso: 'Engenharia de Software',
+        dataInicioVigencia: '01/01/2021',
+        dataFimVigencia: '31/12/2021',
+      },
+    },
+    {
+      id: '4',
+      nome: 'Banco de Dados II',
+      creditos: 2,
+      programa: 'Programa da disciplina',
+      itensBlibliograficos: [
+        'Livro de Engenharia de Software 1',
+        'Livro de Engenharia de Software 2',
+      ],
+      curriculo: {
+        idCurriculo: 100,
+        nomeCurso: 'Engenharia de Software',
+        dataInicioVigencia: '01/01/2021',
+        dataFimVigencia: '31/12/2021',
+      },
+    },
+    {
+      id: '5',
+      nome: 'Intrudução a Inteligência Artificial',
+      creditos: 4,
+      programa: 'Programa da disciplina',
+      itensBlibliograficos: [
+        'Livro de Engenharia de Software 1',
+        'Livro de Engenharia de Software 2',
+      ],
+      curriculo: {
+        idCurriculo: 100,
+        nomeCurso: 'Engenharia de Software',
+        dataInicioVigencia: '01/01/2021',
+        dataFimVigencia: '31/12/2021',
+      },
+    },
+    {
+      id: '6',
+      nome: 'Design de Iteração',
+      creditos: 4,
+      programa: 'Programa da disciplina',
+      itensBlibliograficos: [
+        'Livro de Engenharia de Software 1',
+        'Livro de Engenharia de Software 2',
+      ],
+      curriculo: {
+        idCurriculo: 100,
+        nomeCurso: 'Engenharia de Software',
+        dataInicioVigencia: '01/01/2021',
+        dataFimVigencia: '31/12/2021',
+      },
+    },
+    {
+      id: '7',
+      nome: 'Lógica de Programação',
+      creditos: 4,
+      programa: 'Programa da disciplina',
+      itensBlibliograficos: [
+        'Livro de Engenharia de Software 1',
+        'Livro de Engenharia de Software 2',
+      ],
+      curriculo: {
+        idCurriculo: 100,
+        nomeCurso: 'Engenharia de Software',
+        dataInicioVigencia: '01/01/2021',
+        dataFimVigencia: '31/12/2021',
+      },
+    },
+  ];
+
   return (
     <Grid container gap={1} padding={1} margin={2} flexDirection='column'>
       <Grid item xs={12}>
@@ -25,9 +157,18 @@ export default function Disciplinas() {
         </Breadcrumbs>
       </Grid>
       <Grid item xs={12}>
-        <Container maxWidth='md' sx={{ backgroundColor: '#f5f5f5', paddingTop: "20px", paddingBottom: "20px", borderRadius: "1%", height: ""}}>
+        <Container
+          maxWidth='md'
+          sx={{
+            backgroundColor: '#f5f5f5',
+            paddingTop: '20px',
+            paddingBottom: '20px',
+            borderRadius: '1%',
+            height: '',
+          }}
+        >
           <SearchHeader />
-          <CusmtomAccordion />
+          <CusmtomAccordion disciplinas={disciplinas} />
         </Container>
       </Grid>
     </Grid>
