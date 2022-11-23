7b9734c69f3410cfeec0d5d7584d798e80e002ed
miguel-de-castro <miguel.c@edu.pucrs.br>
2022-11-22 23:41:50 -0300

    WIP

---
+++ src/pages/recursos/components/speedDialCustom.tsx
@@ -0,0 +1,37 @@
+import { SpeedDial, SpeedDialAction } from '@mui/material';
+
+const actions = [
+  { name: 'Adicionar um recurso' },
+  { name: 'Adicionar um novo tipo de recurso' },
+];
+const sla = 'Adicionar Tipo/Recurso';
+
+export default function SpeedDialCustom() {
+  return (
+    <SpeedDial
+      FabProps={{
+        variant: 'extended',
+        style: {
+          height: 50,
+        },
+      }}
+      sx={{ position: 'absolute', bottom: 16, right: 16 }}
+      ariaLabel='SpeedDial basic example'
+      icon={sla}
+    >
+      {actions.map((action) => (
+        <SpeedDialAction
+          FabProps={{
+            variant: 'extended',
+            style: {
+              height: 50,
+            },
+          }}
+          key={action.name}
+          icon={action.name}
+          tooltipTitle={action.name}
+        />
+      ))}
+    </SpeedDial>
+  );
+}
--- src/pages/recursos/recursos.tsx
+++ src/pages/recursos/recursos.tsx
@@ -1,34 +1,51 @@
-import { Breadcrumbs, Grid, Link, Typography, Box, Container } from '@mui/material';
+import { Breadcrumbs, Grid, Link, Typography, Card } from '@mui/material';
 import { useHistory } from 'react-router-dom';
+import SpeedDialCustom from './components/speedDialCustom';
+import RecursosData from './mock/mockRecursos.json';
 
 import { paths } from 'routes/routes';
 
 export default function Recursos() {
   const history = useHistory();
   return (
-    <Grid container gap={1} padding={1} flexDirection='column'>
-      <Grid item>
-        <Breadcrumbs aria-label='breadcrumb'>
-          <Link
-            underline='hover'
-            color='primary'
-            onClick={(e) => {
-              e.preventDefault();
-              history.push(paths.homePage);
-            }}
-          >
-            Página inicial
-          </Link>
-          <Typography color='text.primary'>Recursos</Typography>
-        </Breadcrumbs>
+    <>
+      <Grid container gap={1} padding={1} flexDirection='column'>
+        <Grid item>
+          <Breadcrumbs aria-label='breadcrumb'>
+            <Link
+              underline='hover'
+              color='primary'
+              onClick={(e) => {
+                e.preventDefault();
+                history.push(paths.homePage);
+              }}
+            >
+              Página inicial
+            </Link>
+            <Typography color='text.primary'>Recursos</Typography>
+          </Breadcrumbs>
+        </Grid>
+        <Grid
+          container
+          flexDirection='column'
+          // justifyContent={'space-between'}
+          gap={5}
+          padding={1}
+        >
+          <Grid container flexDirection='row'>
+            <h1>Recursos</h1>
+          </Grid>
+          {RecursosData.map((rec) => {
+            return (
+              <Card>
+                <h3>{rec.name}</h3>
+                <h3>{rec.descricao}</h3>
+              </Card>
+            );
+          })}
+        </Grid>
       </Grid>
-            {/* <Grid>
-        <Container maxWidth='md' sx={{ backgroundColor: '#f5f5f5', paddingTop: "20px", paddingBottom: "20px", borderRadius: "1%"}}>
-          <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
-            <h1>Custom Accordion</h1>
-          </Box>
-        </Container>
-      </Grid> */}
-    </Grid>
+      <SpeedDialCustom />
+    </>
   );
 }
