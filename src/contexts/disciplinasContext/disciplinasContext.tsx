4b1cfe95a4a3eca8eae9bc4103f1172efc6240f7
Bruno Breyer <brunobreyergarcia@gmail.com>
2022-11-24 21:57:28 -0300

    WIP - busca por nome de disciplina

--- src/contexts/disciplinasContext/disciplinasContext.tsx
+++ src/contexts/disciplinasContext/disciplinasContext.tsx
@@ -25,237 +25,11 @@ export interface IDisciplinas {
 }
 
 export const DisciplinasContext = createContext<IDisciplinas>({
-  disciplinas: [
-    {
-      id: '1',
-      nome: 'Construção de Software',
-      creditos: 4,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '2',
-      nome: 'Engenharia Experimental de Software',
-      creditos: 2,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '3',
-      nome: 'Paralela e Distribuida',
-      creditos: 2,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '4',
-      nome: 'Banco de Dados II',
-      creditos: 2,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '5',
-      nome: 'Intrudução a Inteligência Artificial',
-      creditos: 4,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '6',
-      nome: 'Design de Iteração',
-      creditos: 4,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '7',
-      nome: 'Lógica de Programação',
-      creditos: 4,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-  ],
-  store: [
-    {
-      id: '1',
-      nome: 'Construção de Software',
-      creditos: 4,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '2',
-      nome: 'Engenharia Experimental de Software',
-      creditos: 2,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '3',
-      nome: 'Paralela e Distribuida',
-      creditos: 2,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '4',
-      nome: 'Banco de Dados II',
-      creditos: 2,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '5',
-      nome: 'Intrudução a Inteligência Artificial',
-      creditos: 4,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '6',
-      nome: 'Design de Iteração',
-      creditos: 4,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-    {
-      id: '7',
-      nome: 'Lógica de Programação',
-      creditos: 4,
-      programa: 'Programa da disciplina',
-      itensBlibliograficos: [
-        'Livro de Engenharia de Software 1',
-        'Livro de Engenharia de Software 2',
-      ],
-      curriculo: {
-        idCurriculo: 100,
-        nomeCurso: 'Engenharia de Software',
-        dataInicioVigencia: '01/01/2021',
-        dataFimVigencia: '31/12/2021',
-      },
-    },
-  ],
-  setDisciplinas: (disciplinas: IDisciplina[]) => {},
-  searchDisciplinas: (searchField: string) => {},
-  setStore: (disciplinas: IDisciplina[]) => {},
+  disciplinas: [],
+  store: [],
+  setDisciplinas: () => {},
+  searchDisciplinas: () => {},
+  setStore: () => {},
 });
 
 const DisciplinasProvider = ({ children }: { children: ReactNode }) => {
@@ -503,7 +277,13 @@ const DisciplinasProvider = ({ children }: { children: ReactNode }) => {
 
   return (
     <DisciplinasContext.Provider
-      value={{ disciplinas, setDisciplinas, searchDisciplinas, setStore, store }}
+      value={{
+        disciplinas,
+        setDisciplinas,
+        searchDisciplinas,
+        setStore,
+        store,
+      }}
     >
       {children}
     </DisciplinasContext.Provider>
