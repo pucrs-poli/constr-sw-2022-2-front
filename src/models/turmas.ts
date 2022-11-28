49b9198a00b94d01be43d545a50d9f7f43df926a
Draf <fontanadias@gmail.com>
2022-11-28 00:18:56 -0300

    add turmas services and model

---
+++ src/models/turmas.ts
@@ -0,0 +1,8 @@
+export type Turmas = {
+    nome: string;
+    number: number;
+    semester: number;
+    year: number;
+    schdule: string;
+    discipline: string;
+  };
---
+++ src/services/turmas.ts
@@ -0,0 +1,28 @@
+import { Turmas } from 'pages/turmas/turmas';
+import { toRequest } from 'utils/request';
+import { api } from './api';
+import { getEnvironment } from './environment';
+
+const getPrediosSalasEndpoint = () => `${getEnvironment()?.Turmas}`
+
+export const getAllTurmas = () => {
+  const url = `${getPrediosSalasEndpoint()}/building`;
+  // TODO: DESLIGAR O mock
+  return toRequest<Turmas[]>(api.get, [url], 'getTurmas', true);
+};
+
+export const getPredioByID = (id: string) => {
+  const url = `${getTurmasEndpoint()}/turmas/${id}`;
+  // TODO: Desligar o mock
+  return toRequest<Turmas>(api.get, [url], 'getTurma', true);
+};
+
+export const postTurma = () => {
+  return Promise.reject();
+};
+export const putTurma = () => {
+  return Promise.reject();
+};
+export const deleteTurmas = (...id: number[]) => {
+  return Promise.reject();
+};
