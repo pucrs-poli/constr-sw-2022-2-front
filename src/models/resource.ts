6d5d60191b0155dc53a2ff76a8d8be853e8f9c31
AB <ab@ABs-MacBook-Air.local>
2022-11-30 16:29:35 -0300

    adiciona model das reservas

---
+++ src/models/resource.ts
@@ -0,0 +1,17 @@
+export type Detail = {
+  id?: number;
+  name: string;
+};
+
+export type ResourceType = {
+  id?: number;
+  name: string;
+};
+
+export type Resource = {
+  id?: number;
+  description: string;
+  status: string;
+  resourceType: ResourceType;
+  details?: Detail[];
+};
